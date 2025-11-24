93240618f01e6ba2a3c3114fcdf502e8f44450af
orenbass <oren.bassman@elbitsystems.com>
2025-11-24 17:57:54 +0200

    Update app-version.js

--- js/config/app-version.js
+++ js/config/app-version.js
@@ -1,6 +1,6 @@
 // מקור יחיד לגרסת האפליקציה
 (function(){
-  const APP_VERSION = '3.4'; // שנה כאן בלבד לעדכון הגרסה
+  const APP_VERSION = '3.5'; // שנה כאן בלבד לעדכון הגרסה
   // בדף רגיל
   if (typeof window !== 'undefined') {
     window.APP_VERSION = APP_VERSION;
